e67fe0dc5ac098d4a74e8087e0d89d61156ccdb3
clothingmmtimes-lang <clothingmmtimes@gmail.com>
2025-11-15 18:12:46 +0530

    Update and rename script.js to main.js

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+console.log("Homepage loaded");
+
+// Later we will replace this with code that:
+// 1) Calls Cloudflare Worker
+// 2) Gets content from Google Sheets
+// 3) Fills elements like #home_title, #home_subtitle etc.
+
